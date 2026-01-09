e60e2607f4b2f3d28e7237904fbca88e349b10a9
Andressagabriell <andressa_gabriel@outlook.com>
2026-01-08 22:29:02 -0300

    Serve static files from the current directory

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('.'));
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
